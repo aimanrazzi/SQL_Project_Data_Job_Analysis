7bde85886e809c397f5763dfff4bcb39adcd2bf2
aimanrazzi <aimanrazzi1997@gmail.com>
2025-03-17 19:07:32 +0800

    upload sql files

---
+++ project_sql/4_top_paying_skills.sql
@@ -0,0 +1,44 @@
+/*
+Question: What are the top skills based on salary?
+- Look at the average salary associated with each skill for Data Analyst positions.
+- Focuses on roles with specified salaries, regardless of location.
+- Why? It reveals how different skills impact salary levels for Data Analysts and
+    helps identify the most financially rewarding skills to acquire or improve.
+*/
+
+SELECT 
+    skills,
+    ROUND (AVG(salary_year_avg), 2) AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' 
+    AND salary_year_avg IS NOT NULL
+    AND job_location = 'Malaysia'
+GROUP BY
+    skills
+ORDER BY
+    avg_salary DESC
+LIMIT 20;  
+
+/*
+In summary, data analysts in Malaysia who develop specialized skills in tools like 
+Excel, Tableau, and programming languages, coupled with industry-specific knowledge and experience,
+are well-positioned to secure top-paying roles in the field.
+*/
+
+[
+  {
+    "skills": "excel",
+    "avg_salary": "111175.00"
+  },
+  {
+    "skills": "flow",
+    "avg_salary": "111175.00"
+  },
+  {
+    "skills": "tableau",
+    "avg_salary": "111175.00"
+  }
+]
